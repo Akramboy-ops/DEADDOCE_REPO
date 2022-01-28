4e8fb343728e8d13cca91acd9a38971482a573f5
Akramboy-ops <72228375+Akramboy-ops@users.noreply.github.com>
2022-01-28 22:29:57 +0300

    GG

---
+++ loading.py
@@ -0,0 +1,26 @@
+from asyncio import sleep
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.fl ?(.*)')
+async def fakeload(e):
+	inp = e.pattern_match.group(1)
+	load = ["👽","🥶","👽","🥶","👽","🥶","👽","🥶"]
+	bar = ""
+	count = 0
+	await e.edit("`[ТЫ ДОЛБАЁБ?НАХУЙ ТЫ ЭТО ЧИТАЕШЬ?]`")
+	sleep(3)
+	for i in range(13):
+		for division in load:
+			space = " " * (12 - i)
+			await e.edit(f"`{bar}{division}{space}[{count}%]`")
+			count += 1
+			sleep(0.3)
+			if count == 101:
+				break
+		bar += "█"
+	sleep(2)
+	done = "Выгрузка данных админа на сервер завершена!"
+	if inp:
+		done = inp
+	await e.edit(f"`{done}`")
